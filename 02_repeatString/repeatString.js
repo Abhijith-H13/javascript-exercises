cc663d462c031f873330a9bb1c7d3a57e4828533
Abhijith H <abhijithh496@gmail.com>
2022-06-11 21:11:22 +0530

    added repeatstring reversestring solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (s,n) {
+    let temp=s;
+    for (let index = 1; index < n; index++) {
+        temp = temp.concat(s);        
+    }
+    return temp;
 };
 
 // Do not edit below this line
