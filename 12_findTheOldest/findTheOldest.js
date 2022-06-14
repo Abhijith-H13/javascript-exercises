72833df6120608c57b549dd7af01ea4aa4ea1b31
Abhijith H <abhijithh496@gmail.com>
2022-06-14 21:57:58 +0530

    added gettheoldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(a) {
+    let largest = 0;
+    let li=0;
+    for (let index = 0; index < a.length; index++) {
+        d=a[index].yearOfDeath;
+        b=a[index].yearOfBirth;
+        if (!d) {
+            d=new Date().getFullYear();;
+        }age=d-b;
+        if(age>largest){
+            largest=age;
+            li=index;
+        }
+
+    }return a[li];
+
 
 };
 
