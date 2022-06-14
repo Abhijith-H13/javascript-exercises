55f05fc41d2216b3b32d9c827aa654b438490683
Abhijith H <abhijithh496@gmail.com>
2022-06-14 20:55:10 +0530

    solution fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let f=1;
+    let s=1;
+    let t=0;
+    if(n==1||n==2){
+        return 1;
+    }
+    if(n<1){
+        return "OOPS";
+    }
+    while(n>2){
+        t=f+s;
+        f=s;
+        s=t;
+        n=n-1;
+    }return t;
 };
 
 // Do not edit below this line
