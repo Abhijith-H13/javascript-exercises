2418c5fbc608ca5fd50c7d517a46662d0bc678e2
Abhijith H <abhijithh496@gmail.com>
2022-06-15 21:18:52 +0530

    solution caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,17 @@
-const caesar = function() {
 
+const codeSet = code => (code<97?65:97);
+const mod = (n,m)=>(n+m+m)%m;
+const caesar = function(s,n) {
+    let newstr =[];
+    for (let index = 0; index < s.length; index++) {
+        const code = s[index].charCodeAt();
+        if((code>=65&&code<=90)||(code>=97&&code<=122)){
+            newstr[index]=String.fromCharCode(mod(code+n -codeSet(code),26)+codeSet(code)); 
+        }else{
+            newstr[index]=s[index];
+        }        
+    }
+    return newstr.join("");
 };
-
 // Do not edit below this line
 module.exports = caesar;
